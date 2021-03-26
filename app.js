6d0eeac7cb5450244779f2f39592e05809951502
Yusuf Emir Isbilir <yusufemirisbilir@gmail.com>
2021-03-26 12:17:09 +0300

    connected mongoDB

--- app.js
+++ app.js
@@ -1,10 +1,14 @@
 const express = require('express');
 const app = express();
-const port = 3000;
+
+const dotenv = require('dotenv');
+dotenv.config();
+
+const mongoose = require('mongoose');
+mongoose.connect(process.env.DB_CONNECT,{useNewUrlParser:true, useUnifiedTopology:true},()=>{console.log('DB connected ')});
 
 const authRoute = require('./routes/auth');
-
 app.use('/api/user',authRoute);
 
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`));
+app.listen(process.env.PORT, () => console.log(`Port listening`));
