abf203255e6988b3b0fa4e596df13da174043eaf
Yusuf Emir Isbilir <yusufemirisbilir@gmail.com>
2021-03-26 12:49:12 +0300

    register process

--- app.js
+++ app.js
@@ -5,9 +5,17 @@ const dotenv = require('dotenv');
 dotenv.config();
 
 const mongoose = require('mongoose');
-mongoose.connect(process.env.DB_CONNECT,{useNewUrlParser:true, useUnifiedTopology:true},()=>{console.log('DB connected ')});
+mongoose 
+ .connect(process.env.DB_CONNECT, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+        useCreateIndex: true,})   
+ .then(() => console.log("Database connected!"))
+ .catch(err => console.log(err));
 
 const authRoute = require('./routes/auth');
+
+app.use(express.json());
 app.use('/api/user',authRoute);
 
 
--- routes/auth.js
+++ routes/auth.js
@@ -1,8 +1,21 @@
 const router = require('express').Router();
+const User = require('../models/User');
+
 
 // /api/user/register
-router.post('/register',(req,res)=>{
-    res.send('Registered')
+router.post('/register',async (req,res)=>{
+    const user = new User({
+        name:req.body.name,
+        email:req.body.email,
+        password:req.body.password,
+    });
+    try{
+        const savedUser  = await user.save();
+        res.send(savedUser);
+
+    }catch(err){
+        console.log(err);
+    };
 });
 
 module.exports = router;
