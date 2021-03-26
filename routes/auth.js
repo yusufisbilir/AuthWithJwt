673f7e86ce10879a970d39d497daf627bf1dbd69
Yusuf Emir Isbilir <yusufemirisbilir@gmail.com>
2021-03-26 13:05:57 +0300

    password bcrypt

--- routes/auth.js
+++ routes/auth.js
@@ -1,6 +1,7 @@
 const router = require('express').Router();
 const User = require('../models/User');
 const Joi = require('@hapi/joi');
+const bcrypt = require('bcryptjs');
 
 const validateSchema = Joi.object({
     name:Joi.string().min(6).required(),
@@ -14,14 +15,21 @@ router.post('/register',async (req,res)=>{
     const {error} = validateSchema.validate(req.body)
     if(error) return res.status(400).send(error.details[0].message);
 
+    const emailControl = await User.findOne({email:req.body.email});
+
+    if(emailControl) return res.status(400).send('Email adress is used');
+
+    const salt = await bcrypt.genSalt(10);
+    const hashPassword = await bcrypt.hash(req.body.password,salt);
+
     const user = new User({
         name:req.body.name,
         email:req.body.email,
-        password:req.body.password,
+        password:hashPassword,
     });
     try{
         const savedUser  = await user.save();
-        res.send(savedUser);
+        res.send({user:user._id});
 
     }catch(err){
         console.log(err);
