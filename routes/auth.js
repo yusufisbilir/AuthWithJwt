c31566cc6cd39c200b9e6a19da71d08e5915ba97
Yusuf Emir Isbilir <yusufemirisbilir@gmail.com>
2021-03-26 12:56:28 +0300

    validation with joi

--- routes/auth.js
+++ routes/auth.js
@@ -1,9 +1,19 @@
 const router = require('express').Router();
 const User = require('../models/User');
+const Joi = require('@hapi/joi');
 
+const validateSchema = Joi.object({
+    name:Joi.string().min(6).required(),
+    email:Joi.string().min(6).required().email(),
+    password:Joi.string().min(6).required(),
+});
 
 // /api/user/register
 router.post('/register',async (req,res)=>{
+
+    const {error} = validateSchema.validate(req.body)
+    if(error) return res.status(400).send(error.details[0].message);
+
     const user = new User({
         name:req.body.name,
         email:req.body.email,
